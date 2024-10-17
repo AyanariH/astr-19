39e93df810ad9d6456e37f7186d8e4ed76cb1b0c
Ayanari <135068395+AyanariH@users.noreply.github.com>
2024-10-17 15:16:28 -0700

    Add files via upload

---
+++ 1-2.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+f1 = 10.2
+f2 = 12.2
+
+print()
+
+print("f1=10.2")
+print("f2=12.2")
+
+print("Float Sum")
+
+float = f1+f2
+print("f1+f2 = ", float)
+
+print(type(float))
+
+print()
+
+i1 = 4
+i2 = 6
+
+print("i1=4")
+print("i2=6")
+
+print("Integer Difference")
+
+i = i1+i2
+
+print("i1-i2 = ", i1-i2)
+
+print(type(i))
+
+print()
+
+f = 9.4
+i = 12
+
+print("f=9.4")
+print("i=12")
+
+print("Float Integer Product")
+
+p = f*i
+
+print("f*i = ",f*i)
+
+print(type(p))
