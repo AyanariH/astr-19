35f1bc8ea79cba533df046d77160ee058ce511c4
Ayanari <135068395+Samura1sHyper@users.noreply.github.com>
2024-10-17 13:51:05 -0700

    Added 1-1.py
    Prompt #1 of Code Journal #1

---
+++ 1-1.py
@@ -0,0 +1,8 @@
+def PrintPromt():
+	print("My name is Ayanari Hatsusegawa. My pronouns are he him. My favorite movie is Inception and my favorite food is udon.")
+
+def main():
+	PrintPromt()
+
+if __name__=="__main__":
+	main()
